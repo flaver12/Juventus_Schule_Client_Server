67e94445f26cdbd8c07efd5c3e6b7658fcbff082
Flavio Kleiber <flaverkleiber@yahoo.de>
2023-03-25 11:29:41 +0100

    updated class example

--- python-challanges/class.py
+++ python-challanges/class.py
@@ -8,7 +8,23 @@ class DriveBot:
      self.direction = 90
      self.sensor_range = 10
 
+    def ajdust_sensor(self, value):
+       self.sensor_range = value
+
+    def control_bot(self, new_speed, new_direction):
+       self.motorSpeed = new_speed
+       self.direction = new_direction
+
 r2d2 = DriveBot()
+print(r2d2.motorSpeed)
+print(r2d2.direction)
+print(r2d2.sensor_range)
+
+r2d2.ajdust_sensor(10)
+r2d2.control_bot(20, 180)
+
+print('===============')
+
 print(r2d2.motorSpeed)
 print(r2d2.direction)
 print(r2d2.sensor_range)
