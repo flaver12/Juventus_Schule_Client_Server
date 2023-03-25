bb12470a771a2f7a294c191eacf105def1e9ed97
Flavio Kleiber <flaverkleiber@yahoo.de>
2023-03-25 11:24:36 +0100

    class example v2

--- python-challanges/class.py
+++ python-challanges/class.py
@@ -1,7 +1,12 @@
 class DriveBot:
-    motorSpeed = 5
-    direction = 90
-    sensor_range = 10
+    motorSpeed = 0
+    direction = 0
+    sensor_range = 0
+
+    def __init__(self) -> None:
+     self.motorSpeed = 5
+     self.direction = 90
+     self.sensor_range = 10
 
 r2d2 = DriveBot()
 print(r2d2.motorSpeed)
